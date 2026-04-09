f4f5d3e7c1105ab1967e7c85f296d51a82bddf76
EnioAmarantes <enio.amarantes@gmail.com>
2026-04-08 22:58:25 -0300

    feat: Enhance router configuration with navigation error handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,23 @@
 import { ApplicationConfig, importProvidersFrom, APP_ID } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withNavigationErrorHandler } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from '@app/interceptors/auth.interceptor';
 
+const navigationErrorHandler = (error: unknown) => {
+  console.error('Navigation Error:', error);
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: APP_ID, useValue: 'brain-health-app' },
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withNavigationErrorHandler(navigationErrorHandler)
+    ),
     provideAnimations(),
     provideHttpClient(
       withInterceptors([authInterceptor])
