d49e5965948794ccfade88a654c1680e528771c8
EnioAmarantes <enio.amarantes@gmail.com>
2026-04-08 22:57:34 -0300

    feat: Add APP_ID provider to application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, APP_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -8,6 +8,7 @@ import { authInterceptor } from '@app/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_ID, useValue: 'brain-health-app' },
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(
